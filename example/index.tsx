299cdc04abdc93b9588f7d098a4261cd0532dc01
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-11-11 09:50:32 +0200

    refactor:

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,17 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { ToggleSwitch } from '@snapdev-ui/capital-art-ds';
+import '@snapdev-ui/capital-art-ds/dist/tailwind.css';
 
 const App = () => {
+  const [enabled, setEnabled] = React.useState(false);
+
   return (
     <div>
-      <Thing />
+      <ToggleSwitch
+        enabled={enabled}
+        onChange={(value: boolean) => setEnabled(value)}
+      />
     </div>
   );
 };
