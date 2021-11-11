f6a4ac8bb705ca35504ae6d0132ad02712c50356
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-11-11 11:54:28 +0200

    refactor:

--- src/ToggleSwitch/ToggleSwitch.tsx
+++ src/ToggleSwitch/ToggleSwitch.tsx
@@ -4,20 +4,35 @@ import { Switch } from '@headlessui/react';
 export interface ToggleSwitchProps {
   enabled: boolean;
   onChange: (value: Boolean) => void;
+  switchH?: number;
+  switchW?: number;
+  toggleTranslateX?: number;
+  toggleH?: number;
+  toggleW?: number;
 }
 
-const ToggleSwitch = ({ enabled, onChange }: ToggleSwitchProps) => (
+const ToggleSwitch = ({
+  enabled,
+  onChange,
+  switchH = 6,
+  switchW = 11,
+  toggleTranslateX = 5,
+  toggleH = 5,
+  toggleW = 5,
+}: ToggleSwitchProps) => (
   <Switch
     checked={enabled}
     onChange={onChange}
     className={`${enabled ? 'bg-green-500' : 'bg-gray-400'}
-          relative inline-flex flex-shrink-0 h-7 w-14 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          relative inline-flex flex-shrink-0 h-${switchH} w-${switchW} border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
   >
-    <span className="sr-only">Use setting</span>
+    <span className="sr-only">Switch</span>
     <span
       aria-hidden="true"
-      className={`${enabled ? 'translate-x-7' : 'translate-x-0'}
-            pointer-events-none inline-block h-6 w-6 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+      className={`${
+        enabled ? `translate-x-${toggleTranslateX}` : 'translate-x-0'
+      }
+            pointer-events-none inline-block h-${toggleH} w-${toggleW} rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
     />
   </Switch>
 );
